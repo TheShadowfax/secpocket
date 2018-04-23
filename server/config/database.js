e1fb7517d33619c322645a454b2e0dc7ddff82e8
Shrujan Kumar Chilukunuri <schiluku@opentext.com>
2018-04-23 16:32:39 +0530

    ServerAuth Completion

---
+++ server/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://secpocketuser:9182goodbye@ds155299.mlab.com:55299/secpocket',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
